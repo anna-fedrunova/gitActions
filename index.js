129630c9e0d8b259f839a12b3a8ba3f0ec0fa252
anna-fedrunova <a.fedrunova@gmail.com>
2021-09-17 18:52:26 +0300

    my first action

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const http = require('http');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('<h1>Hello, World</h1>');
-    }
-});
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
